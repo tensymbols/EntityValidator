7c0fccea9b5badf6cf59e0e29d15328e89fe387e
tensymbols <thetensymbols@gmail.com>
2023-04-17 15:25:46 +0300

    new rules

--- EntityValidator.go
+++ EntityValidator.go
@@ -18,6 +18,9 @@ func UserValidate(nickname string, email string) error {
 	if len(nickname) >= 100 || nickname == "" {
 		return errors.New("invalid user nickname")
 	}
+	if len(email) >= 40 {
+		return errors.New("invalid user email")
+	}
 	s := strings.Split(email, "@")
 	if len(s) < 2 || s[0] == "" || s[1] == "" {
 		return errors.New("invalid user email")
